cf37f10141a9785058bff8dbbbe3138cff76ce9a
Vala Arefi <vala.arefi@gmail.com>
2021-05-11 18:35:54 +0200

    Add color

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './logo.svg';
+import RandomColorGenerator from './RandomColorGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomColorGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RandomColorGenerator.js
@@ -0,0 +1,51 @@
+import './App.css';
+import randomColor from 'randomcolor';
+import React, { useState } from 'react';
+
+const RandomColorGenerator = () => {
+  const [color, setColor] = useState('#000'); // initial variable
+  // const color = randomColor();
+  const changeColor = () => {
+    setColor(randomColor);
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      <button onClick={changeColor}>Change Color</button>
+    </div>
+  );
+};
+
+export default RandomColorGenerator;
+
+// import React, { useState } from 'react';
+// import { SketchPicker } from 'react-color';
+
+// export default function;
+
+// class Component extends React.Component {
+
+//   render() {
+//     return <SketchPicker />
+//   }
+// }
+
+// RandomColorGenerator() {
+//   const { randomcolor, setrandomcolor } = useState(color.random().color);
+//   return (
+//     <div>
+//       <div>(color)</div>
+//       <button> onClick={() => setColor('red')}>Update Color</button>
+//     </div>
+//   );
+// }
+// {/* <div class="input-color">
+//   <input type="text" value="Orange" />
+//   <div class="color-box" style="background-color: #FF850A;"></div>
+//   // Replace "#FF850A" to change the color
+// </div>; */}
+
+// -----------------
